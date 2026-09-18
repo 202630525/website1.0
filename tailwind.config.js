5b5fc549dd747499dc6bc0a20bf10c61ed9f0eda
202630525 <202630525@eonju.ms.kr>
2026-09-18 10:52:07 +0900

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        glass: {
+          light: "rgba(255, 255, 255, 0.18)",
+          border: "rgba(255, 255, 255, 0.3)",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
